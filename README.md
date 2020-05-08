33b9c985950b2674b7ce962c50746e280a423ae6
Robert Perez <robgaray9111@yahoo.com>
2020-05-08 15:02:21 -0700

    Setting up project

---
+++ README.md
@@ -0,0 +1,7 @@
+Building a CLI to create projects rapidly with a uniform method
+
+Going to be using this for freelancing
+
+Option to use a template that just has provides a static site or a site with a CMS backend (Strapi) for easy API creation
+
+Going to try and make it all CLI as its for personal use, if need for a front end happens later then i'll do so.
---
+++ index.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+
+console.log("Setting up project from templates. Prepare to answer some questions!");
+console.log("What is the name of your Project?");
+console.log("Which template should I use?");
+console.log("Does it require a backend?")
+
+
+//Get access to file system
+const copyDest = () => {
+
+    
+}
+
+//pick template numerically
+
+//copy to new location
