e06ef8d063a7a862cbb13e01892d0c90025005e9
Robert Perez <robgaray9111@yahoo.com>
2020-05-08 16:01:57 -0700

    Created function to find root then template path

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
+// Base imports
 const fs = require('fs');
 const path = require('path');
+const inquirer = require('inquirer');
 
-console.log("Setting up project from templates. Prepare to answer some questions!");
+// Important Consts
+const baseTemplatePath = path.join(__dirname, 'templates');
+const choice = 'backend';
+
+/* console.log("Setting up project from templates. Prepare to answer some questions!");
 console.log("What is the name of your Project?");
 console.log("Which template should I use?");
-console.log("Does it require a backend?")
+console.log("Does it require a backend?"); */
 
+//Get current path
+const currentDir = () => {
+    return path.resolve();
+}
 
 //Get access to file system
 const copyDest = () => {
@@ -13,8 +23,12 @@ const copyDest = () => {
 }
 
 //pick template numerically
-const pickTemplate = () => {
-
+const pickTemplate = (option) => {
+    const choice = new Map([
+        [1, 'backend'],
+        [2, 'no-backend']
+    ]);
+    return template = path.join(baseTemplatePath, choice.get(option));
 }
 
 //copy to new location
@@ -22,3 +36,5 @@ const pickTemplate = () => {
 const createInDest = () => {
     
 }
+
+console.log(pickTemplate(2));
