ce45f8188dedca40e3982c403e628f4a01f672c7
Robert Perez <robgaray9111@yahoo.com>
2020-07-26 15:21:59 -0700

    Removed Async as it was causing promise issues. Made Sync.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Base imports
-const inquirer = require("inquirer");
-const { copyDest, pickTemplate } = require("./logic");
+const inquirer = require('inquirer')
+const { copyDest, pickTemplate } = require('./logic')
 
 /* 
 Done - "What is the name of your Project?"
@@ -10,23 +10,22 @@ Done - "Which template should I use?"
 inquirer
 	.prompt([
 		{
-			type: "input",
-			name: "dirName",
-			message: "What is the name of the business?",
+			type: 'input',
+			name: 'dirName',
+			message: 'What is the name of the business?',
 		},
 		{
-			type: "list",
-			name: "template",
-			message: "Which template would you like to use?",
-			choices: ["backend", "no-backend"],
+			type: 'list',
+			name: 'template',
+			message: 'Which template would you like to use?',
+			choices: ['backend', 'no-backend'],
 		},
 	])
 	.then((answers) => {
-		const { dirName, template } = answers;
-		copyDest(dirName);
-		pickTemplate(template);
-		//console.info("Business Name:", dirName);
+		const { dirName, template } = answers
+		copyDest(dirName)
+		pickTemplate(template)
 	})
 	.catch((err) => {
-		if (error) return console.error(err);
-	});
+		if (error) return console.error(err)
+	})
--- logic.js
+++ logic.js
@@ -1,56 +1,48 @@
-const path = require("path");
-const fs = require("fs-extra");
-const homedir = require("os").homedir();
+const path = require('path')
+const fs = require('fs-extra')
+const homedir = require('os').homedir()
 
 // Important Consts
-const baseTemplatePath = path.join(path.join(__dirname, "templates"));
-let copyLocationDir = "";
+const baseTemplatePath = path.join(path.join(__dirname, 'templates'))
+let copyLocationDir = ''
 
 // Wait function to allow us to get dir name into variable
 async function wait() {
-	await new Promise((resolve) => setTimeout(resolve, 1000));
-
-	return 5;
+	await new Promise((resolve) => setTimeout(resolve, 1000))
+	return 5
 }
 
 //Get access to file system
-exports.copyDest = async (name) => {
+exports.copyDest = (name) => {
 	try {
-		copyLocationDir = path.join(
-			homedir,
-			"Documents",
-			"business",
-			`${name}`
-		);
+		copyLocationDir = path.join(homedir, 'Documents', 'business', `${name}`)
 
-		await fs.ensureDir(
-			path.join(homedir, "Documents", "business", `${name}`)
-		);
+		fs.ensureDir(path.join(homedir, 'Documents', 'business', `${name}`))
 
-		console.log(`Creating directory at "${copyLocationDir}"`);
+		console.log(`Creating directory at "${copyLocationDir}"`)
 	} catch (err) {
-		console.error(err);
+		console.error(err)
 	}
-};
+}
 
 //pick template
-exports.pickTemplate = async (option) => {
+exports.pickTemplate = (option) => {
 	// Set up path string, ex: 'quickbusinesstemp/template/backend'
-	const template = path.join(baseTemplatePath, option);
+	const template = path.join(baseTemplatePath, option)
 	try {
 		//Create folder in destination
-		wait().then(createInDest(template));
+		createInDest(template)
 	} catch (err) {
-		console.error(err);
+		console.error(err)
 	}
-};
+}
 
 //Creates folder w/ files needed to develop
-const createInDest = async (directory) => {
+const createInDest = (directory) => {
 	try {
 		//Copy template directory AKA backend - no backend to the selected
-		await fs.copy(directory, copyLocationDir);
+		fs.copySync(directory, copyLocationDir)
 	} catch (err) {
-		console.error(err);
+		console.error(err)
 	}
-};
+}
